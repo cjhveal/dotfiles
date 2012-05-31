9a855acdf4c96df0987e50b982b0dc0206a56a96
Cody Veal <cveal05@gmail.com>
2012-05-31 10:12:59 -0400

    adding new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -48,6 +48,10 @@ command -v mvim >/dev/null 2>&1 && {
 alias reload!='. ~/.zshrc'
 
 alias a='ack -ia'
+alias oo='open .'
 
 alias ip="curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g'"
 alias localip='ipconfig getifaddr en0'
+alias whois="whois -h whois-servers.net"
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
