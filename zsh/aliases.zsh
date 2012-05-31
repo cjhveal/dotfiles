7592e792166a69b2b1a5a8a91b504676f4823848
Cody Veal <cveal05@gmail.com>
2012-05-31 10:21:58 -0400

    remove conflicting options from ack alias, ackrc handles default options

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -47,7 +47,7 @@ command -v mvim >/dev/null 2>&1 && {
 
 alias reload!='. ~/.zshrc'
 
-alias a='ack -ia'
+alias a='ack'
 alias oo='open .'
 
 alias ip="curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g'"
