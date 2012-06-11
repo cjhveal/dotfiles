99ae655cf95f8a586b96688c4286131f53d4dcd9
Cody Veal <cveal05@gmail.com>
2012-06-11 09:03:31 -0400

    adding partial word and substring completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,5 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# matches case insensitive for lowercase, then partial word, then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
