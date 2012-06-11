5b7cc3292395ffa9cfd48ee38b1cf3017f462eec
Cody Veal <cveal05@gmail.com>
2012-06-11 09:03:18 -0400

    adding ssh keygen alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -50,6 +50,7 @@ alias localip='ipconfig getifaddr en0'
 alias whois="whois -h whois-servers.net"
 alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+alias mksshkey="ssh-keygen -b 8192 -t rsa -C '$1' -f ~/.ssh/$1.rsa"
 
 function murder () {
   ps | grep $1 | grep -v grep | awk '{print $1}' | xargs kill -9
