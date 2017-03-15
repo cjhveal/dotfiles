9815939c31ce8959ca7bbcfb085c2075a687327d
cjhveal <cveal05@gmail.com>
2017-03-15 02:55:29 -0700

    whitespace significant

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,18 @@ command_exists() {
   hash "$1" 2>/dev/null
 }
 
-if !command_exists brew; then
+if ! command_exists brew; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew tap Homebrew/bundle
 brew bundle -v --file="brew/.Brewfile"
 
-if !command_exists stow; then
+if ! command_exists stow; then
   brew install stow
 fi
+
+
+
 
 
