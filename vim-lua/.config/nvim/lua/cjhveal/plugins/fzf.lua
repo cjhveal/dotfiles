7b33864ccce3d0bde92c402c2ef3b54de88cf929
Cody Veal <cveal05@gmail.com>
2025-06-12 16:17:52 -0400

    add more fzf commands

--- vim-lua/.config/nvim/lua/cjhveal/plugins/fzf.lua
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/fzf.lua
@@ -4,23 +4,33 @@ return {
   dependencies = { "echasnovski/mini.icons" },
   opts = {},
   keys = {
-    {"<leader><space>", "<cmd>FzfLua files<CR>", desc = "Find Files"},
-    {"<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find Files"},
-    {"<leader>fg", "<cmd>FzfLua git_files<CR>", desc = "Find Git Files"},
-    {"<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<CR>", desc="Switch Buffer"},
+    -- find
+    { "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<CR>", desc="Switch Buffer" },
+    { "<leader><space>", "<cmd>FzfLua files<CR>", desc = "Find Files" },
+    { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>FzfLua git_files<CR>", desc = "Find Git Files" },
+    { "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
 
+    -- git
+    { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
+    { "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Status" },
+
+    -- search
     { '<leader>s"', "<cmd>FzfLua registers<cr>", desc = "Registers" },
     { "<leader>sa", "<cmd>FzfLua autocmds<cr>", desc = "Auto Commands" },
+    { "<leader>sb", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
     { "<leader>sc", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
     { "<leader>sC", "<cmd>FzfLua commands<cr>", desc = "Commands" },
-    
+    { "<leader>sd", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document Diagnostics" },
+    { "<leader>sD", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
+    { "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Grep (live)" },
     { "<leader>sh", "<cmd>FzfLua help_tags<cr>", desc = "Help Pages" },
     { "<leader>sH", "<cmd>FzfLua highlights<cr>", desc = "Search Highlight Groups" },
     { "<leader>sj", "<cmd>FzfLua jumps<cr>", desc = "Jumplist" },
     { "<leader>sk", "<cmd>FzfLua keymaps<cr>", desc = "Key Maps" },
     { "<leader>sl", "<cmd>FzfLua loclist<cr>", desc = "Location List" },
     { "<leader>sM", "<cmd>FzfLua man_pages<cr>", desc = "Man Pages" },
-    { "<leader>sm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" }
-
+    { "<leader>sm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" },
+    { "<leader>sq", "<cmd>FzfLua quickfix<cr>", desc = "Quickfix List" },
   }
 }
