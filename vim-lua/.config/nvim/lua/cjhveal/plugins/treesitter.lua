b1f91614a81557e6c9e509ad86a96ce78b5a46aa
Cody Veal <cveal05@gmail.com>
2025-06-12 12:58:34 -0400

    fix treesitter config, add default parsers

--- vim-lua/.config/nvim/lua/cjhveal/plugins/treesitter.lua
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/treesitter.lua
@@ -2,5 +2,42 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   branch = 'master',
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  opts = {
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false
+    },
+    indent = { enable = true },
+    sync_install = true,
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+  },
+  config = function (_, opts)
+    require('nvim-treesitter.configs').setup(opts)
+  end
 }
