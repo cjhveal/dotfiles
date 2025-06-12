46c736904b6b58789de67dbc3dedcc6f858a0f6e
Cody Veal <cveal05@gmail.com>
2025-06-12 15:52:46 -0400

    improve lsp

--- vim-lua/.config/nvim/lua/cjhveal/plugins/lsp.lua
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/lsp.lua
@@ -1,19 +1,122 @@
+---@param on_attach fun(client:vim.lsp.Client, buffer)
+---@param name? string
+function on_attach(on_attach, name)
+  return vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf ---@type number
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client and (not name or client.name == name) then
+        return on_attach(client, buffer)
+      end
+    end,
+  })
+end
+
+
 return {
+  {
+    "neovim/nvim-lspconfig",
+    diagnostics = {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        spacing = 4,
+        source = "if_many",
+        prefix = "●",
+        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+        -- prefix = "icons",
+      },
+      severity_sort = true,
+    },
+    opts = {
+      inlay_hints = {
+        enabled = true,
+        exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+      },
+      servers = {
+        vtsls = {
+          -- explicitly add default filetypes, so that we can extend
+          -- them in related extras
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                maxInlayHintLength = 30,
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+        },
+      },
+      setup = {
+        vtsls = function(_, _)
+          -- do something here?
+        end
+      },
+    },
+    config = function(_, opts)
+      vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+      local servers = opts.servers
+      for server, server_opts in pairs(servers) do
+        if opts.setup[server] then
+          if opts.setup[server](server, server_opts) then
+            return
+          end
+        end
+
+        require("lspconfig")[server].setup(server_opts)
+      end
+    end
+  },
   {
     "mason-org/mason.nvim",
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "prettierd",
+      }
+    }
   },
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = {
-          "lua_ls",
-          "jsonls",
-          "tailwindcss",
-          "vtsls",
-        },
+      ensure_installed = {
+        "lua_ls",
+        "jsonls",
+        "tailwindcss",
+        "vtsls",
+      },
     },
     dependencies = {
       {"mason-org/mason.nvim", opts={}},
