93a9b09c307ae90c8d28f9332392d6ecc9ff4a3f
Cody Veal <cveal05@gmail.com>
2025-06-12 12:59:00 -0400

    add gruvbox as backup colorscheme

--- vim-lua/.config/nvim/lua/cjhveal/plugins/colors.lua
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/colors.lua
@@ -1,4 +1,7 @@
 return {
-	"loctvl842/monokai-pro.nvim",
-	opts = {filter = "classic"}
+  {
+    "loctvl842/monokai-pro.nvim",
+    opts = {filter = "classic"}
+  },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , opts = {}}
 }
