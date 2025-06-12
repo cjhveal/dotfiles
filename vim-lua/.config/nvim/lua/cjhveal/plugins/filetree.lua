f29023b7fe40d5c692c1c2aac23d63b319ba5ca0
Cody Veal <cveal05@gmail.com>
2025-06-12 01:45:42 -0400

    add neo tree

---
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/filetree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  cmd = "NeoTree",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  }
+}
