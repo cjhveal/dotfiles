5662b32ba26dea8b3e7048adf7c845af5a42d642
Cody Veal <cveal05@gmail.com>
2025-06-11 23:34:53 -0400

    add blink completion, add lsp

---
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/lsp.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { "lua_ls", "vtsls" },
+    },
+    dependencies = {
+      {"mason-org/mason.nvim", opts={}},
+      "neovim/nvim-lspconfig",
+    },
+  },
+}
