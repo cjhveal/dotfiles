3a90686121d75fc418e18f811b19f66293f9a16c
Cody Veal <cveal05@gmail.com>
2025-06-15 11:57:32 -0400

    add statusline extensions, show relative paths in filenames

--- vim-lua/.config/nvim/lua/cjhveal/plugins/statusline.lua
+++ vim-lua/.config/nvim/lua/cjhveal/plugins/statusline.lua
@@ -1,6 +1,19 @@
 return {
   'nvim-lualine/lualine.nvim',
   opts = {
-    options = {theme = 'powerline'}
+    options = {
+      theme = 'powerline',
+      extensions = {'fzf', 'man', 'mason', 'neo-tree', 'trouble'},
+    },
+    sections = {
+      lualine_c = {
+        { "filename", path = 1},
+      },
+    },
+    inactive_sections = {
+      lualine_c = {
+        { "filename", path = 1},
+      },
+    }
   },
 }
